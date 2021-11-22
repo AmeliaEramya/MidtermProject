f28524f5d4b9ede296e7d146fe3e22d9cc416dcb
AmeliaEramya <92102516+AmeliaEramya@users.noreply.github.com>
2021-11-21 19:30:05 -0500

    Final changes 7:29am 11/21/2021

---
+++ Migrations/202111212250045_DateOfBirth.cs
@@ -0,0 +1,16 @@
+namespace MidtermProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DateOfBirth : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
